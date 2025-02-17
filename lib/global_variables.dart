0a258b10cff5c400019cc6554073ec0b33f49c2d
Al Rahim <alrahim018@gmail.com>
2025-02-17 19:09:13 +0600

    Created shop_app completely..

---
+++ lib/global_variables.dart
@@ -0,0 +1,34 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Men\'s Nike Shoes',
+    'price': 44.52,
+    'company': 'Nike',
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/shoes_1.png',
+  },
+  {
+    'id': '1',
+    'title': 'Addidas Shoes',
+    'price': 20.12,
+    'company': 'Addidas',
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/shoes_2.png',
+  },
+  {
+    'id': '2',
+    'title': 'Bata Men\'s Nike Shoes',
+    'price': 28.95,
+    'company': 'Bata',
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/shoes_3.png',
+  },
+  {
+    'id': '3',
+    'title': 'Jordan Shoes',
+    'price': 420.52,
+    'company': 'Nike',
+    'sizes': [8, 9, 10, 11],
+    'imageUrl': 'assets/images/shoes_4.png',
+  },
+];
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/cart_provider.dart';
+import 'package:shop_app/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => CartProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Shopping App',
+        theme: ThemeData(
+          useMaterial3: true,
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromRGBO(254, 206, 1, 1),
+            primary: const Color.fromRGBO(254, 206, 1, 1),
+          ),
+          appBarTheme: const AppBarTheme(
+              titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+          )),
+          inputDecorationTheme: const InputDecorationTheme(
+            hintStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+            prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+          ),
+          textTheme: const TextTheme(
+            titleLarge: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+            ),
+            titleMedium: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+            bodySmall: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        home: const HomePage(),
+      ),
+    );
+  }
+}
